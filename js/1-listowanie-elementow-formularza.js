f675df0c9186bf1961c1b02040e15ee3b770a84f
Wujek <mjwojcik94@gmail.com>
2017-02-11 14:05:06 +0100

    zrobioned

--- js/1-listowanie-elementow-formularza.js
+++ js/1-listowanie-elementow-formularza.js
@@ -6,11 +6,19 @@ $(function() {
 
 */
 
-document.getElementsByName("submit").addEventListener('click', przeszukaj )
-
 function przeszukaj() {
    
-   var all = document.getElementsByTagName("*");
-   console.log(all);
-
+   var all = document.getElementsByTagName("form")[0].childNodes;
+      
+   all.forEach(function(element) {
+      if(element.name == 'imie') console.log(element.value);
+      if(element.name == 'nazwisko') console.log(element.value);      
+   });
 }
+
+document.getElementsByName("wyslij")[0].addEventListener('click',  function(e){
+   e.preventDefault();
+   przeszukaj();
+   console.log('  >> przeszukiwanie : 100%')
+} );
+
