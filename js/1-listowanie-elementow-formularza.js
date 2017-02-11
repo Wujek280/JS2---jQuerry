8c05197d726aaac257ca19c93ef9545b18fafc9c
Wujek <mjwojcik94@gmail.com>
2017-02-11 13:26:56 +0100

    init form

---
+++ js/1-listowanie-elementow-formularza.js
@@ -0,0 +1,16 @@
+'use strict';
+/*
+
+$(function() {
+   });
+
+*/
+
+document.getElementsByName("submit").addEventListener('click', przeszukaj )
+
+function przeszukaj() {
+   
+   var all = document.getElementsByTagName("*");
+   console.log(all);
+
+}
