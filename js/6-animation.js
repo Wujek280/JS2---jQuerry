0278075034fb13d727cc90f33470309c508a0f6e
Wujek <mjwojcik94@gmail.com>
2017-02-11 11:53:06 +0100

    onload funkcje

--- js/6-animation.js
+++ js/6-animation.js
@@ -1,2 +1,22 @@
 'use strict';
-$()
+var flaga_schowaj=false;
+
+function schowaj() {
+
+   $('p').hide(5000, function() {
+      if(flaga_schowaj == false) {
+         console.log(">>Koniec.");
+         flaga_schowaj=true;
+      } 
+   }
+    ).show(1000);
+}
+
+
+
+$(document).ready( function() {
+   
+   $('#go').click( schowaj );
+   
+});
+
