93b513fc17b3bf4cdb9f3ef047e36333efca5844
Wujek <mjwojcik94@gmail.com>
2017-02-12 10:11:08 +0100

    twerkin'

--- js/2-kallukator-odsetek.js
+++ js/2-kallukator-odsetek.js
@@ -1,10 +1,29 @@
 'use strict';
 
+
+function obliczZysk(wplata, iloscLat, okresKapitalizacji, oprocentowanie, podatek) {
+   
+   var oprocentowanieOpodatkowane = oprocentowanie;
+   if(podatek == true) {oprocentowanieOpodatkowane *= (1-.19);}
+   
+   var Ko = parseFloat(wplata); 
+   var n = iloscLat*okresKapitalizacji;
+   var base = (1+oprocentowanieOpodatkowane/okresKapitalizacji);
+   var exp = Math.pow(base, n);
+   var kapital = Ko*exp;
+   
+   kapital=kapital.toFixed(2);
+   return kapital;
+      
+}
+
 function oblicz() {
+   
    console.log("słowo testowe na d.");
    
    var wplata = document.getElementById("hajs");
    wplata = wplata.value;
+   wplata = parseInt(wplata);
    console.log(wplata,' PLN');
    
    var okresKapitalizacji =document.getElementById("kapitalizacja");
@@ -29,47 +48,11 @@ function oblicz() {
    console.log(' ',wynik, 'PLN');
    
    var kapitalKoncowyParagraf = document.querySelectorAll("p > span")[0];
-      //   var odsetkiParagraf = document.querySelectorAll("p > span")[1];
-      //   var odsetki = document.querySelectorAll("p > span")[3];
 
    kapitalKoncowyParagraf.innerHTML = wynik;
-      //odsetkiParagraf.innerHTML = (wynik - wplata).toFixed(2) ;
  }
-
-function obliczZysk(wplata, iloscLat, okresKapitalizacji, oprocentowanie, podatek) {
-   
-   var oprocentowanieOpodatkowane = oprocentowanie;
-   if(podatek == true) {oprocentowanieOpodatkowane *= (1-.19);}
-   
-   var Ko = parseFloat(wplata); 
-   var n = iloscLat*okresKapitalizacji;
-   var base = (1+oprocentowanieOpodatkowane/okresKapitalizacji);
-   var exp = Math.pow(base, n);
-   var kapital = Ko*exp;
-   
-   kapital=kapital.toFixed(2);
-   return kapital;
-      
-}
-
-
-
-
 
 
 document.getElementById("przelicz").addEventListener('click', function(e){
-   //e.preventDefault();
    oblicz();
 } );
-
-//document.getElementById('wynik').innerHTML
-   
-   /*
-    if(podatek == true) { 
-         var answer = (ans-wplata)*0.81;
-         answer += Ko;
-         return answer.toFixed(2);
-      }else{
-         return ans.toFixed(2);
-   
-      }*/
